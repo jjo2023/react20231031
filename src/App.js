61a6097e3cc6df1e88b7e5864950fe19e5740eef
jjo2023 <snowman7685@gmail.com>
2023-10-31 12:21:16 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+function MyComponent1() {
+    return(
+    <div>
+        <h1>Hello Component</h1>
+    </div>
+    )
+}
 
+function MyComponent2() {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Soluta?</li>
+                <li>Sint?</li>
+            </ul>
+        </>
+    );
+}
 
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        {/*
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+        */}
+        <MyComponent2 />
+        {/*
+         <p>lorem dsds </p>
+         <ul>
+         <li>lorem</li>
+         <li>lorem</li>
+         <li>lorem</li>
+         </ul>
+        */}
     </div>
+
   );
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
